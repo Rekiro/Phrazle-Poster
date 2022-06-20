781c83d6d3f4c856de89644ed2852640ca61461f
Rekiro <81427486+Rekiro@users.noreply.github.com>
2022-06-20 03:02:56 +0000

    Added a custom status

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ client = commands.Bot(command_prefix='!')  #put your own prefix here
 
 @client.event
 async def on_ready():
+  await client.change_presence(status = discord.Status.idle, activity = discord.Game('!meaning'))
   print("I'm alive.")  #will print "bot online" in the console when the bot is online
 
 
