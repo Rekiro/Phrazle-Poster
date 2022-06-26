564939e7d39401e498aa817c6be0a874a4f9f468
Rekiro <81427486+Rekiro@users.noreply.github.com>
2022-06-26 05:08:12 +0000

    Changed client class with bot class. Enabled the bot to respond to other commands.

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from discord.ext import commands#, tasks
 #from discord.ext.commands import has_permissions, CheckFailure, check
 #^ basic imports for other features of discord.py and python ^
 
-client = discord.Client()
+#client = discord.Client()
 
-client = commands.Bot(command_prefix='!')  #put your own prefix here
+bot = commands.Bot(command_prefix='!')  #put your own prefix here
 
 
-@client.event
+@bot.event
 async def on_ready():
-  await client.change_presence(status = discord.Status.idle, activity = discord.Game('!meaning'))
-  print("I'm alive.")  #will print "bot online" in the console when the bot is online
+  await bot.change_presence(status = discord.Status.idle, activity = discord.Game('!meaning'))
+  print("I'm alive.")  #will print "I'm alive" in the console when the bot is online
 
 
-@client.command()
+@bot.command()
 async def hi(ctx):
-  await ctx.send("Hello " +ctx.author.mention)  #simple command so that when you type "!ping" the bot will respond with "pong!"
+  await ctx.reply("Hello, please use `!meaning <word/phrase>` for definitions." +ctx.author.mention)  #simple command so that when you type "!ping" the bot will respond with "pong!"
 
 
 # instantiate Lexico class from search_lexico.py
@@ -31,14 +31,14 @@ dictionary = search_lexico.Lexico()
 no_result_message = '''Sorry, we can\'t find what you are searching for. But you can search on web here:\n--> https://www.oxfordlearnersdictionaries.com/definition/english/dictionary'''
 
 
-@client.event
+@bot.listen('on_message')
 async def on_message(message):
-  if message.author == client.user:
+  if message.author == bot.user:
     return
     
   message_content = message.content.lower()  # lower case message
 
-  if f'!meaning' in message_content:
+  if f'!meaning' or '!m' in message_content:
 
     key_words, phrase = dictionary.key_words_search_words(message_content)
     Meaning, Examples = dictionary.search(key_words)
@@ -65,10 +65,12 @@ async def on_message(message):
     else:
       await message.channel.send(no_result_message)
 
+#  await bot.process_commands(message)
+
 
 
 try:
-  client.run(os.getenv('TOKEN'))
+  bot.run(os.getenv('TOKEN'))
 except:
   os.system("kill 1")
 
